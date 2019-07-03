d107fbb06aa0a9c5d0a3bd029d080f9fba327dfa
DaegiKim <rmrhtdms@gmail.com>
2019-07-04 05:27:42 +0900

    practice functional programming

---
+++ src/functional_programming/MapReduceApp.java
@@ -0,0 +1,16 @@
+package functional_programming;
+
+import java.util.Arrays;
+
+public class MapReduceApp {
+    public static void main(String[] args) {
+        String s = "hello world. these are lower cases.";
+
+        String reduce = Arrays
+                .stream(s.split(""))
+                .map(String::toUpperCase)
+                .reduce("", (a, b) -> a + b);
+
+        System.out.println(reduce);
+    }
+}
---
+++ src/functional_programming/references.md
@@ -0,0 +1,5 @@
+Java 스트림 Stream (1) 총정리
+https://futurecreator.github.io/2018/08/26/java-8-streams/
+
+Java 8 Stream Tutorial
+https://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
